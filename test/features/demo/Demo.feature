8d303b0c1e42bb095a9e4bad3df71bf4b5cb1f4e
mihaelacepraga <mihaelacepraga@gmail.com>
2022-06-17 15:46:25 +0300

    add input handling

--- test/features/demo/Demo.feature
+++ test/features/demo/Demo.feature
@@ -1,13 +1,12 @@
 Feature: Feature name
 
-   @demo
     Scenario Outline: Run first demo feature
         Given Google page is opened
         When Search with <SearchItem>
         Then Click on the first test result
-        # Then URL should match <ExpectedURL>
+        Then URL should match <ExpectedURL>
 
 
         Examples:
-            | TestID      | SearchItem| ExpectedURL             |
-            | DEMO_TC001  | WDIO      | https://webdriver.io/   |
+            | TestID     | SearchItem | ExpectedURL           |
+            | DEMO_TC001 | WDIO       | https://webdriver.io/ |
--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -1,5 +1,6 @@
 import { Given, When, Then } from "@wdio/cucumber-framework";
 import chai from "chai";
+        //    demo feature
 
 Given(/^Google page is opened$/ ,async function() {
     await browser.url("https://www.google.com/")
@@ -13,13 +14,39 @@ When(/^Search with (.*)$/ ,async function(searchItem) {
     await elem.setValue(searchItem)
     await browser.keys("Enter")
 })
+
 Then(/^Click on the first test result$/, async function () {
     let elem = await $('<h3>')
     elem.click()
 })
+
 Then(/^URL should match(.*)$/, async function(ExpectedURL) {
     let url = await browser.getUrl()
     chai.expect(url).to.equal(ExpectedURL)
+
+    
+})
+
+        //   web interactions
+Given(/^A web page is opened$/, async function () {
+    await browser.url("/inputs")
+    await browser.setTimeout({implicit: 5000, pageLoad: 1000})
+    await browser.maximizeWindow()
+    
+})
+When( /^Perform web interactions$/, async function () {
+    let num = 12345
+    let strNum = num.toString()
+    let elem = $("[type=number]")
+    await elem.click()
+
+    for (let i = 0; i < strNum.length; i++) {
+        let charStr = strNum.charAt(i);
+        await browser.pause(1000)
+        await browser.keys(charStr)
+    
+    }
+    await browser.debug()
 
     
 })
